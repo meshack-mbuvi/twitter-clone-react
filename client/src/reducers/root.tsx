33c72b875fdc32efa2dee21457cc81374497b01a
mbuvi <meshack.mbuvi@andela.com>
2019-01-01 21:59:50 +0300

    Revert "New user"

--- client/src/reducers/root.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { User } from "./auth.reducer";
-import { Twits } from "./twits.reducer";
-
-export const rootReducer = combineReducers({
-  userReducer: User,
-  twitReducer: Twits
-});
